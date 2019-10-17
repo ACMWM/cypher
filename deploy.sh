0089086eae49db4dd30e81fd5c9defe4f474b626
Jacob Adams <jacob@tookmund.com>
2019-10-17 19:32:35 -0400

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+DEPLOY="/home/cypher"
+
+# Preferably separate commits but this ensures changes are pushed
+git commit -a
+git pull
+git push
+
+rsync -rcvz --delete --exclude='.git*' --exclude='.zfs' \
+	--exclude='deploy.sh' --exclude='README.md' --exclude='CNAME' \
+	./ "bg3.cs.wm.edu:$DEPLOY/"
