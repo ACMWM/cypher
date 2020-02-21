7bd2a3c497f5b7a13ca5b2d11ebba3d06ebde4de
Jacob Adams <jacob@tookmund.com>
2020-02-21 17:22:48 -0500

    Update schedule from google docs

---
+++ script.js
@@ -0,0 +1,23 @@
+function init() {
+  Tabletop.init(
+	  {
+		  key: "https://docs.google.com/spreadsheets/d/1TFrRrBa1fd-Mh-SounwxlfwRn67wNXdau9yHVSZrgQc/pubhtml",
+		  simpleSheet: true
+	  }
+  ).then(function(data, tabletop) {
+	  console.log(data);
+	  for(var i in data) {
+		  d = data[i];
+		  let row = document.createElement("TR");
+		  for (var j in d) {
+			  if (j == "date") continue;
+			  let td = document.createElement("TD");
+			  td.innerHTML = d[j];
+			  row.appendChild(td);
+		  }
+		  let t = document.getElementById(d["date"]);
+		  t.appendChild(row);
+	  }
+  })
+}
+window.addEventListener("DOMContentLoaded", init)
