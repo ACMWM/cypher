699e3274ab2865c81b684adb97106e512735d1fb
Joseph Lee <jo112095@gmail.com>
2021-01-31 20:57:46 -0500

    changed back to original script

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ git push
 
 rsync -rcvz --delete --exclude='.git*' --exclude='.zfs' \
 	--exclude='deploy.sh' --exclude='README.md' --exclude='CNAME' \
-	./ "joegen@th121-7.cs.wm.edu:$DEPLOY/"
+	./ "bg3.cs.wm.edu:$DEPLOY/"
