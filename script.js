71bdb8c9dd220de6e032eec83b1ea715bf99a7d9
Jackson <jpkubin@wm.edu>
2023-03-10 17:29:58 -0500

    add schedule

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
-function init() {
-  Tabletop.init(
-	  {
-		  key: "https://docs.google.com/spreadsheets/d/1Zm89-OlunMpeOoMLjmbANzT2NJVojafUjbcXT99nZiw/edit?usp=sharing/pubhtml",
-		  simpleSheet: true
-	  }
-  ).then(function(data, tabletop) {
-	  console.log(data);
-	  for(var i in data) {
-		  d = data[i];
-		  let row = document.createElement("TR");
-		  for (var j in d) {
-			  if (j == "date") continue;
-			  let td = document.createElement("TD");
-			  td.innerHTML = d[j];
-			  row.appendChild(td);
-		  }
-		  let t = document.getElementById(d["date"]);
-		  t.appendChild(row);
-	  }
-  })
+function parse(file, options) {
+	return new Promise((complete, error) => {
+		Papa.parse(file, { ...options, complete, error });
+	});
 }
-window.addEventListener("DOMContentLoaded", init)
 
+function EventElements(data) {
+	const info = document.createElement('tr');
+
+	const time = document.createElement('td');
+	time.innerText = data.Time;
+	const title = document.createElement('td');
+	title.innerText = data.Title;
+	const location = document.createElement('td');
+	location.innerText = data.Location;
+	info.append(time, title, location);
+
+	if (!data.Details) return [info];
+
+	const detailsRow = document.createElement('tr');
+	detailsRow.classList.add('details');
+
+	const details = document.createElement('td');
+	details.setAttribute('colspan', 3);
+	details.innerText = data.Details;
+	detailsRow.append(details);
+
+	return [info, detailsRow];
+}
+
+(async function () {
+	const {data} = await parse(
+		"https://docs.google.com/spreadsheets/d/e/2PACX-1vQ85UWX8YgueGqmEsfdwoFKlozGIuMe6zmlSpdV92TtOa-2L0b-FGrRGLoTs5x5hfq4S2YKvpKWYfjM/pub?gid=0&single=true&output=csv",
+		{
+			download: true,
+			header: true,
+		}
+	);
+	data.sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
+	const dayTables = new Map();
+	for (let event of data) {
+		const day = event.Date.toLowerCase();
+		let table = dayTables.get(day);
+		if (table === undefined) {
+			table = document.querySelector(`#${day}-schedule`);
+			dayTables.set(day, table ?? null);
+		}
+		if (table) {
+			table.append(...EventElements(event))
+			console.log(event)
+		}
+	}
+	
+})();
 
